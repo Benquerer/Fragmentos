0754de8f4c85a9ef33a9dfe8418b9a0c95de7381
pauloagsantos <pauloagsantos@gmail.com>
2024-10-11 17:51:50 +0100

    versão C

--- FragmentosC/app/src/main/java/ipt/dam/fragmentosb/fragmentos/Fragmento2.kt
+++ FragmentosC/app/src/main/java/ipt/dam/fragmentosb/fragmentos/Fragmento2.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import ipt.dam.fragmentosb.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -16,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Fragmento1.newInstance] factory method to
+ * Use the [Fragmento2.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Fragmento1 : Fragment() {
+class Fragmento2 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,8 +35,7 @@ class Fragmento1 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragmento1, container, false);
-
+        return inflater.inflate(R.layout.fragment_fragmento2, container, false)
     }
 
     companion object {
@@ -48,12 +45,12 @@ class Fragmento1 : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragmento1.
+         * @return A new instance of fragment Fragmento2.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Fragmento1().apply {
+            Fragmento2().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
